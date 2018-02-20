9ad359955cfdeb18572c8162db6cdb031e6ee9aa
Preston Sego <LPSego3+dev@gmail.com>
2018-02-20 10:26:44 -0500

    eh?

--- src/decorators/tracked.ts
+++ src/decorators/tracked.ts
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 export function tracked(target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
   return {
+    configurable: true,
+    enumerable: false,
     set: function(this: React.Component, newValue: any) {
-      this.setState({ [name]: newValue });
+      this.setState({ [propertyKey]: newValue });
     },
     get: function(this: React.Component): any {
-      return this.state[name];
+      return this.state[propertyKey];
     }
   }
 }
