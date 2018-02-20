cee680c0505c35f87a77dac53dd560cc7de882b2
Preston Sego <LPSego3+dev@gmail.com>
2018-02-20 15:31:14 -0500

    asuto

--- tests/decorators/tracked.tsx
+++ tests/decorators/tracked.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
 
-class TrackedTest extends React.Component {
-  @tracked property: string;
+import { tracked } from '../../src/decorators/tracked';
+
+declare interface IProps {};
+
+declare interface IState {
+  property: string,
+  someNumber: number
+};
+
+class TrackedTest extends React.Component<IProps, IState> {
+  @tracked
+  property = '';
+
+  @tracked
+  someNumber = 0;
+
+  @tracked('property')
+  get trackedGetter() {
+    return this.property + ' hi';
+  }
+
+  // @tracked('someNumber')
+  computedNumber = {
+    get(this: TrackedTest): number {
+      return this.someNumber;
+    },
+    set(this: TrackedTest, newValue: number): void {
+      this.someNumber = newValue;
+    } 
+  }
 
   componentDidMount() {
     this.property = 'did mount';
